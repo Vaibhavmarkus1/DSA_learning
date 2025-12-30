3f2ccc6ae1871d2957fcf913eb14b039e5214a63
Pandey <vaibhpandey@deloitte.com>
2025-12-30 17:33:51 +0530

    Added Diameter of trees

--- Day_1/Data Structure/Tree/constructure.java
+++ Day_1/Data Structure/Tree/constructure.java
@@ -1,5 +1,6 @@
 import java.util.Stack;
 import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Queue;
 import java.math.*;
 public class constructure {
@@ -26,6 +27,12 @@ public class constructure {
         }  
     }
 
+    public static class DiaPair {
+    
+        int ht;
+        int dia;
+        
+    }
     public static void display(Node root){
 
         if(root==null){
@@ -131,9 +138,200 @@ public class constructure {
 
     }
 
+    //State wise decision -- 1>pre, 2>in, 3>post
+    public static void IterativePreOrder(Node head){
+
+        String prestr = "";
+        String posStr = "";
+        String inStr = "";
+        
+        Pair rp = new Pair(head, 1);
+        Stack<Pair> st = new Stack<>();
+        st.push(rp);
+
+        while(st.size()>0){
+
+            Pair tp = st.peek();
+            if(tp.state==1){
+                prestr+=tp.node.data+" ";
+                 tp.state++;
+
+                if(tp.node.left!=null){    
+                    Pair np = new Pair(tp.node.left, 1);
+                    st.push(np);
+                    
+                }
+               
+            }
+            else if (tp.state == 2){
+                inStr+=tp.node.data+" ";
+                tp.state++;
+
+                if(tp.node.right!=null){
+                    Pair np = new Pair(tp.node.right, 1);
+                    st.push(np);
+                    
+                }
+                
+            }
+            else{
+                    posStr+=tp.node.data+" ";
+                    st.pop();
+                
+
+            }
+        }
+        System.out.println(prestr);
+        System.out.println(inStr);
+        System.out.println(posStr);
+
+    }
 
 
-    public static void main(String[] args) {
+    public static  ArrayList<Node> al =new ArrayList<>();
+
+    public static boolean nodeToRoot(Node head, int target){
+        if(head == null){
+            return false;
+        }
+
+        if(head.data == target){
+            al.add(head);
+            return true;
+        }
+
+        boolean lf = nodeToRoot(head.left, target);
+        if(lf){
+            al.add(head);
+            return true;
+        }
+
+        boolean rf = nodeToRoot(head.right, target);
+        if(rf){
+            al.add(head);
+            return true;
+        }
+        return false;
+    }
+
+    public static void printKLevelDown(Node head, int k, Node blocker){
+        
+        if(head==null || k<0 || head==blocker){
+            return;
+        }
+        if(k==0){
+            System.out.println(head.data);
+        }
+
+        printKLevelDown(head.left,k-1,blocker);
+        printKLevelDown(head.right,k-1,blocker);
+    }
+    
+   public static void kDistanceFar(Node head, int data, int k){
+
+     nodeToRoot(head, data);
+     ArrayList<Node> ntp = al;
+
+    for(int i = 0; i<ntp.size();i++){
+        printKLevelDown(ntp.get(i), k-i, i==0?null:ntp.get(i-1));
+    }
+
+   }
+
+   public static void pathToLeafFromNode(Node head, String path, int sum, int hi, int lo){
+   
+    if(head==null){
+        return;
+    }
+
+    if(head.left ==null && head.right == null){
+
+        sum +=head.data;
+        path+=head.data;
+        if(sum<=hi && sum>=lo){
+            System.out.println(sum);
+            System.out.println(path);
+        }
+        return;
+    }
+
+
+    pathToLeafFromNode(head.left, path + head.data + " ", sum+head.data, hi,lo);
+    pathToLeafFromNode(head.right, path + head.data + " ", sum+head.data, hi,lo);
+
+   }
+    
+   public static Node createLeftCloneTree(Node head){
+
+    if(head == null ){
+        return null;
+    }
+
+    Node lcr = createLeftCloneTree(head.left);
+    Node rcr = createLeftCloneTree(head.right);
+
+    Node nn = new Node(head.data,lcr,null);
+    head.left = nn;
+    head.right = rcr;
+    
+     return head;
+   }
+
+   public static void printSingleChildNode(Node child, Node parent){
+
+    if(child == null){
+        return;
+    }
+    if(parent!=null && parent.left == child && parent.right == null){
+        System.out.println(child.data);
+    }
+    else if(parent!=null && parent.right==child && parent.left ==null){
+        System.out.println(child.data);
+    }
+    printSingleChildNode(child.left, child);
+    printSingleChildNode(child.right, child);
+
+   }
+
+   public static Node removeLeaves(Node node){
+
+    if(node == null){
+        return null;
+    }
+
+    if(node.left == null && node.right == null){
+        return null;
+    }
+
+    node.left = removeLeaves(node.left);
+    node.right = removeLeaves(node.right);
+    return node;
+   }
+
+
+   public static DiaPair Diameter(Node node){
+     
+    if(node ==null){
+        DiaPair bp = new DiaPair();
+        bp.ht=-1;
+        bp.dia = 0;
+        return bp;
+    }
+    DiaPair lp = Diameter(node.left);
+    DiaPair rp = Diameter(node.right);
+
+    DiaPair mp = new DiaPair();
+
+    mp.ht = Math.max(lp.ht,rp.ht) +1;
+
+    int fes = lp.ht + rp.ht +2;
+    mp.dia = Math.max(fes,Math.max(lp.dia, rp.dia));
+
+    return mp;
+
+   }
+   
+   public static void main(String[] args) {
         Integer[] arr = {50,25,12,null,null,37,30,null,null,null,75,62,null,70,null,null,87,null,null};
         int idx = 0;
 
@@ -188,8 +386,22 @@ public class constructure {
         // System.out.println(height(root));
 
        // preOrder(root);
-        levelOrder(root);
+        //levelOrder(root);
+        //IterativePreOrder(root);
+        //nodeToRoot(root, 30);
+        //System.out.println(al);
+        // printKLevelDown(root, 2);
+        //kDistanceFar(root,25, 1);
+        //pathToLeafFromNode(root,"",0,200,20);
+        //Node cn = createLeftCloneTree(root);
+        //display(cn);
+        //printSingleChildNode(root, null);
+        //display(root);
+        //Node testNode = removeLeaves(root);
+        //display(testNode);
 
+        DiaPair dp = Diameter(root);
+        System.out.println(dp.dia);
     }
     
 }
